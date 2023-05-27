00aef965fdb16da70522713b1ceffea3b89748f7
Einer Durango <einer743dc@gmail.com>
2023-05-27 17:27:54 -0500

    Models working, admin panel implemented

--- project/apps/drug/admin.py
+++ project/apps/drug/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+# class DrugAdmin
+@admin.register(models.Drug)
+class DrugAdmin(admin.ModelAdmin):
+    list_display = ['name', 'company', 'composition', 'package_qty', 'drugtype', 'restricted', 'stock', 'price']
+    search_fields = ['name', 'drugtype__name']
+
+# class DrugTypeAdmin
+@admin.register(models.DrugType)
+class DrugTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+# class CompanyAdmin
+@admin.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country', 'phone', 'mail']
+    search_fields = ['name', 'country__name']
+
+# class CountryAdmin
+@admin.register(models.Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
--- project/apps/drug/models.py
+++ project/apps/drug/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+
+# Country model
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+
+    def __str__(self):
+        return self.name
+
+# Company model
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+    phone = models.CharField(max_length=100)
+    mail = models.EmailField()
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+
+    def __str__(self):
+        return self.name
+
+# DrugType model
+class DrugType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Drug Type'
+        verbose_name_plural = 'Drug Types'
+
+    def __str__(self):
+        return self.name
+
+# Drug model
+class Drug(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
+    composition = models.CharField(max_length=255)
+    package_qty = models.IntegerField()
+    drugtype = models.ForeignKey(DrugType, on_delete=models.SET_NULL, null=True)
+    restricted = models.BooleanField(default=False)
+    stock = models.IntegerField()
+    price = models.IntegerField()
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Drug'
+        verbose_name_plural = 'Drugs'
+
+    def __str__(self):
+        return self.name
+    
